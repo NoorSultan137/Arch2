5aeb41db5a697c3a30cef9a13f07295199a170df
NoorSultan137 <155090640+NoorSultan137@users.noreply.github.com>
2024-02-21 13:48:20 +0200

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,6 +1,7 @@
 #include "exec.h"
 #include "colors.h"
 
+#include <sys/types.h> // I think that this is a missing header, beacause pid_t is not working properly without it
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,13 +23,8 @@ static const char *randomColor() {
     int color;
 
     // BEGIN STUDENT IMPLEMENTATION
+    color = rand() % numColors;
 
-    do {
-        color = rand() % numColors;
-    } while (color == lastColor);
-
-    lastColor = color;
-    
     // END STUDENT IMPLEMENTATION
 
     return colors[color];
@@ -38,16 +34,19 @@ static const char *randomColor() {
 static void child(char **argv, const int *pipefd) {
 
     // BEGIN STUDENT IMPLEMENTATION
+    close(pipefd[0]); // Close the read end of the pipe
 
-    close(pipefd[0]); // close read end of the pipe
-    dup2(pipefd[1], STDOUT_FILENO); // redirect stdout to the pipe
-    dup2(pipefd[1], STDERR_FILENO); // redirect stderr to the pipe
-    close(pipefd[1]); // close the write end of the pipe
+    if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
+        perror("dup2");
+        exit(errno);
+    }
 
-    execvp(argv[0], argv); // execute the process
+    close(pipefd[1]); // Close the write end of the pipe
 
-    // if execvp fails, print an error message in red
-    fprintf(stderr, "%s%s: %s\n%s", RED, argv[0], strerror(errno), RESET);
+    execvp(argv[0], argv);
+    // If execvp returns, it means an error occurred
+    perror("execvp");
+    exit(errno);
     // END STUDENT IMPLEMENTATION
 
     exit(errno);
@@ -61,20 +60,20 @@ static void readPipe(const int *pipefd) {
     char ch;
 
     // BEGIN STUDENT IMPLEMENTATION
-
-    const char *currentColor = randomColor();
+    const char *currentColor = NULL;
     while (read(pipefd[0], &ch, 1) > 0) {
         if (isgraph(ch)) {
-            printf("%s%c", currentColor, ch);
-            fflush(stdout);
-            currentColor = randomColor();
-        } else {
-            printf("%c", ch);
-            fflush(stdout);
+            const char *nextColor = randomColor();
+            if (currentColor != nextColor) {
+                currentColor = nextColor;
+                printf("%s", currentColor);
+            }
         }
+        printf("%c", ch);
+        fflush(stdout);
     }
-    printf("%s", RESET); // reset color at the end
-}
+
+    close(pipefd[0]); // Close the read end of the pipe
     // END STUDENT IMPLEMENTATION
 }
 
@@ -85,15 +84,16 @@ static void parent(char *cmd, pid_t pid, const int *pipefd) {
     int status = 0; // store the exit status of the child
 
     // BEGIN STUDENT IMPLEMENTATION
-
-    close(pipefd[1]); // close write end of the pipe
-
-    // handle input from the pipe
+    close(pipefd[1]); // Close the write end of the pipe
     readPipe(pipefd);
+    close(pipefd[0]); // Close the read end of the pipe
 
-    // wait for the child to terminate
+    int status = 0; // store the exit status of the child
     waitpid(pid, &status, 0);
 
+    // check the exit status of the child and print an error message if needed
+    if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+        error(cmd, WEXITSTATUS(status)); // error exit status
     // END STUDENT IMPLEMENTATION
 
     // check the exit status of the child and print an error message if needed
@@ -105,30 +105,23 @@ static void parent(char *cmd, pid_t pid, const int *pipefd) {
 void exec(char **argv) {
 
     // BEGIN STUDENT IMPLEMENTATION
-
     int pipefd[2];
-    pid_t pid;
-
-    // create the pipe
     if (pipe(pipefd) == -1) {
         perror("pipe");
-        return;
+        exit(errno);
     }
 
-    // fork the process
-    pid = fork();
+    pid_t pid = fork();
     if (pid == -1) {
         perror("fork");
-        close(pipefd[0]);
-        close(pipefd[1]);
-        return;
+        exit(errno);
     }
 
     if (pid == 0) {
-        // in child process
+        // Child process
         child(argv, pipefd);
     } else {
-        // in parent process
+        // Parent process
         parent(argv[0], pid, pipefd);
     }
     // END STUDENT IMPLEMENTATION
