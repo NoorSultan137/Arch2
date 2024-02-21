832518cacdb8120d0a6d58c43e71a1df6fb0258f
NoorSultan137 <155090640+NoorSultan137@users.noreply.github.com>
2024-02-21 16:09:41 +0200

    Update vish.c

--- vish.c
+++ vish.c
@@ -14,15 +14,17 @@ const char *cd(char **tokens, const char *prompt) {
 
     // BEGIN STUDENT IMPLEMENTATION
 
-    if (tokens[1] != NULL) {
-        if (chdir(tokens[1]) != 0) {
-            perror("cd");
-        } else {
-            prompt = getPrompt();
-        }
+    const char *newPrompt = prompt;
+
+    if (tokens[1] == NULL) {
+        fprintf(stderr, "vish: cd: missing argument\n");
+    } else if (tokens[2] != NULL) {
+        fprintf(stderr, "vish: cd: too many arguments\n");
     } else {
-        printf("cd: missing argument\n");
+        newPrompt = tokens[1];
     }
+
+    return newPrompt;
     // END STUDENT IMPLEMENTATION
 
     tokens = NULL; // suppress unused parameter warning
@@ -61,4 +63,49 @@ int main() {
     freeLine();
 
     return EXIT_SUCCESS;
+}
+
+
+//I defined the 4 functions initLine(), freeLine(), getPrompt(), getLine(), since they are not defined anywhere in the program.
+
+// Static variables to store prompt and colors
+static const char *prompt = "> ";
+static const char *color1 = RED;
+static const char *color2 = GREEN;
+static const char *color3 = YELLOW;
+static const char *color4 = BLUE;
+static const char *color5 = BOLD_RED;
+
+void initLine(const char *c1, const char *c2, const char *c3, const char *c4, const char *c5) {
+    color1 = c1;
+    color2 = c2;
+    color3 = c3;
+    color4 = c4;
+    color5 = c5;
+}
+
+void freeLine() {
+    // You may add cleanup logic here if needed
+}
+
+const char *getPrompt() {
+    return prompt;
+}
+
+const char *getLine(const char *prompt) {
+    char *line = NULL;
+    size_t len = 0;
+
+    printf("%s", prompt);
+    if (getline(&line, &len, stdin) == -1) {
+        free(line);
+        return NULL;
+    }
+
+    // Remove newline character if present
+    if (line[strlen(line) - 1] == '\n') {
+        line[strlen(line) - 1] = '\0';
+    }
+
+    return line;
 }
